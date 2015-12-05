61ee1a44444477fb9a66b7f643696fcc1f0ac5ca
ebenp <eben.pendleton@gmail.com>
2015-12-05 11:14:28 -0500

    Corrected futures

--- code/adventday1.py
+++ code/adventday1.py
@@ -1,9 +1,9 @@
-if __name__ == '__main__':
-    from __future__ import absolute_import, division, print_function
-    from builtins import (bytes, str, open, super, range,
+from __future__ import absolute_import, division, print_function
+from builtins import (bytes, str, open, super, range,
                       zip, round, input, int, pow, object)
-    from future import standard_library
-    standard_library.install_aliases()
+from future import standard_library
+standard_library.install_aliases()
+if __name__ == '__main__':
     with open('input1.txt', "r") as myfile:
         # strip new lines just in case
         data=myfile.read().replace('\n', '')
